e5a16bcc097a392430f1135adc68103535ba5866
Philipp Riedel <riedel.philipp@gmx.net>
2022-05-18 11:56:50 +0200

    adds new task and show task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,36 @@
 class TasksController < ApplicationController
+  # before_action :set_task, only: [:show]
+
   def index
-    # @tasks = tasks.all
+    @tasks = Task.all
+    # raise
   end
+
+  def show
+    @task = Task.find(params[:id])
+    # raise
+  end
+
+  def new
+    @task = Task.new # Needed to instantiate the form_for
+  end
+
+  def create
+    # @task = Task.new(params[:task]) # Will raise ActiveModel::ForbiddenAttributesError
+    @task = Task.new(task_params)
+    @task.save
+    # No need for app/views/tasks/create.html.erb
+    redirect_to tasks_path(@task)
+  end
+
+  private
+
+  # input of forms is forbidden to save to the DB by default, so we need #task_params
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
+  # def set_task
+  #   @task = Task.find(params[:id])
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get "tasks", to: "tasks#index"
+  get "tasks/new", to: "tasks#new"
+  get "tasks/:id", to: "tasks#show", as: "show"
+  post "tasks", to: "tasks#create"
 end
